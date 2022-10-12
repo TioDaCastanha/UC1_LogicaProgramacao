1bce177bbc4a28c9cfcc44663bd348d4ea6d0f13
Tio da Castanha <almosttherealan@gmail.com>
2022-10-12 20:46:56 -0300

    Add Pilha e Fila files

--- src/PilhaFila/Pilha.java
+++ src/PilhaFila/Pilha.java
@@ -1,4 +1,25 @@
 package PilhaFila;
 
+import java.util.Stack;
+
 public class Pilha {
+    public static void main(String[] args){
+
+        Stack<String> pilha = new Stack<>();
+
+        //Pode se usar .push ou .addElement
+        pilha.push("Verde");
+        pilha.push("Azul");
+        pilha.push("Laranja");
+
+        System.out.println(pilha);
+
+        System.out.println(pilha.peek());
+
+        System.out.println(pilha);
+        //.peek só mostra e .pop mostra e remove o topo.
+        System.out.println(pilha.pop());
+
+        System.out.println(pilha);
+    }
 }
--- src/PilhaFila/fila.java
+++ src/PilhaFila/fila.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class fila {
     public static void main(String[] args){
 
-        Queue<String> fila = new LinkedList<String>();
+        Queue<String> fila = new LinkedList<>();
 
         fila.add("Verde");
         fila.add("Azul");
@@ -15,8 +15,21 @@ public class fila {
 
         System.out.println(fila);
 
-        //Pode se usar o .peek ou .element (peek retorna null e element da exception se a lista estiver vazia).
-        System.out.println("Atendimento: "+ fila.element());
+        //Pode se usar o .peek, .element ou .poll
+
+        //peek retorna null e element da exception se a lista estiver vazia mas não removem o primeiro elemento.
+        System.out.println("Atendimento com Peek/Element: "+ fila.peek());
+
+        System.out.println(fila);
+
+        //Pool remove o primeiro elemento e mostra.
+        System.out.println("Atendimento com Pool: "+ fila.poll());
+
+        System.out.println(fila);
+
+        System.out.println("Atendimento com Pool: "+ fila.poll());
+
+        System.out.println(fila);
     }
 
 }
